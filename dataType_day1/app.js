25bb951aecceceffa688d9d7ae4f7bbded65b386
arushi shukla <arushi.shukla@tothenew.com>
2017-01-11 15:25:04 +0530

    Hexcode to Rgb Converter Created in app.js

--- dataType_day1/app.js
+++ dataType_day1/app.js
@@ -2,6 +2,8 @@
  * Created by arushi on 10/1/17.
  */
 
+// returning true array type
+
 var functionType = function(datatype){
 
     if(Array.isArray(datatype) === true)
@@ -23,3 +25,35 @@ var functionType = function(datatype){
 }
 
 functionType();
+
+// returning hex code into rgb
+
+var hexRgb = function(hexcolor){
+
+
+    var hexCode = hexcolor.split('#')[1];
+    console.log(hexCode.length)
+
+    var invalid = "invalid Color Code";
+
+    if(hexCode.length>6)
+    {
+        return invalid;
+    }
+
+    else
+    {console.log(hexCode)
+        var codeArr =[]
+        for(var i=0;i<3;i++)
+        {
+            var y = i+2;
+            var slicedCode = hexCode.slice(i,y);
+            codeArr.push(parseInt(slicedCode,16))
+
+        }
+        console.log(codeArr)
+
+    }
+
+
+}
